4e669ae0e82a51d8a6164849be00c8d71d49a8a2
unknown <zziinnaaro@yahoo.com>
2023-09-05 14:12:50 +0300

    Init commit

---
+++ mongodb.js
@@ -0,0 +1,30 @@
+// CRUD - create, read, update, delete
+
+// const mongodb = require("mongodb")
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require("mongodb")
+
+const connectionURL = "mongodb://127.0.0.1:27017"
+const databaseName = "task-manager"
+
+// const id = new ObjectID()
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error){
+      return console.log("Unable to connect to database!")
+    }
+
+    const db = client.db(databaseName)
+
+   db.collection("tasks").deleteOne({
+    description: "clean the house"
+   }).then((response)=>{
+    console.log(response)
+   }).catch((error)=>{
+    console.log(error)
+   })
+})
